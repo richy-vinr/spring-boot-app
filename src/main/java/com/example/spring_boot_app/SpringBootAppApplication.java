b18ac41518d32497ff562d42c58a22a75d8595c0
Richy Kalingarayar <richy@vinr.eu>
2026-02-18 17:49:50 +0100

    MongoDB added to test secrets

--- src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
+++ src/main/java/com/example/spring_boot_app/SpringBootAppApplication.java
@@ -2,12 +2,12 @@ package com.example.spring_boot_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class SpringBootAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppApplication.class, args);
+    }
 }
